3607aa39be02d2d0fd383137ef149311a286b8f9
Sean Musgrave <sean.musgrave@maine.edu>
2020-07-21 20:36:49 -0400

    Changed width and height back

--- MDK-ARM/SPI.c
+++ MDK-ARM/SPI.c
@@ -6,8 +6,8 @@
 //DC pin -> PE10
 
 extern SPI_HandleTypeDef hspi1;
-volatile uint16_t LCD_HEIGHT = 480;
-volatile uint16_t LCD_WIDTH	 = 320;
+volatile uint16_t LCD_HEIGHT = 320;
+volatile uint16_t LCD_WIDTH	 = 480;
 
 
 
@@ -41,11 +41,11 @@ void SPI_Init(void)
 
 void LCD_Reset(void)
 {
-HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_RESET);
-HAL_Delay(200);
-HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
-HAL_Delay(200);
-HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_SET);	
+	HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_RESET);
+	HAL_Delay(200);
+	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
+	HAL_Delay(200);
+	HAL_GPIO_WritePin(LCD_RST_PORT, LCD_RST_PIN, GPIO_PIN_SET);	
 }
 
 void LCD_Enable(void)
@@ -73,61 +73,61 @@ void LCD_Set_Address(uint16_t X1, uint16_t Y1, uint16_t X2, uint16_t Y2)
 
 void LCD_Draw_Color(uint16_t Color)
 {
-//SENDS COLOR
-unsigned char TempBuffer[2] = {Color>>8, Color};	
-HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_SET);	
-HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
-HAL_SPI_Transmit(HSPI_INSTANCE, TempBuffer, 2, 1);	
-HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_SET);
+	//SENDS COLOR
+	unsigned char TempBuffer[2] = {Color>>8, Color};	
+	HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_SET);	
+	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(HSPI_INSTANCE, TempBuffer, 2, 1);	
+	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_SET);
 }
 
-//INTERNAL FUNCTION OF LIBRARY
+
 /*Sends block colour information to LCD*/
 void LCD_Draw_Color_Burst(uint16_t Color, uint32_t Size)
 {
-//SENDS COLOUR
-uint32_t Buffer_Size = 0;
-if((Size*2) < BURST_MAX_SIZE)
-{
-	Buffer_Size = Size;
-}
-else
-{
-	Buffer_Size = BURST_MAX_SIZE;
-}
-	
-HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_SET);	
-HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
-
-unsigned char chifted = 	Color>>8;;
-unsigned char burst_buffer[Buffer_Size];
-for(uint32_t j = 0; j < Buffer_Size; j+=2)
+	//SENDS COLOUR
+	uint32_t Buffer_Size = 0;
+	if((Size*2) < BURST_MAX_SIZE)
 	{
-		burst_buffer[j] = 	chifted;
-		burst_buffer[j+1] = Color;
+		Buffer_Size = Size;
+	}
+	else
+	{
+		Buffer_Size = BURST_MAX_SIZE;
+	}
+		
+	HAL_GPIO_WritePin(LCD_DC_PORT, LCD_DC_PIN, GPIO_PIN_SET);	
+	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_RESET);
+
+	unsigned char chifted = 	Color>>8;;
+	unsigned char burst_buffer[Buffer_Size];
+	for(uint32_t j = 0; j < Buffer_Size; j+=2)
+		{
+			burst_buffer[j] = 	chifted;
+			burst_buffer[j+1] = Color;
+		}
+
+	uint32_t Sending_Size = Size*2;
+	uint32_t Sending_in_Block = Sending_Size/Buffer_Size;
+	uint32_t Remainder_from_block = Sending_Size%Buffer_Size;
+
+	if(Sending_in_Block != 0)
+	{
+		for(uint32_t j = 0; j < (Sending_in_Block); j++)
+			{
+			HAL_SPI_Transmit(HSPI_INSTANCE, (unsigned char *)burst_buffer, Buffer_Size, 10);	
+			}
 	}
 
-uint32_t Sending_Size = Size*2;
-uint32_t Sending_in_Block = Sending_Size/Buffer_Size;
-uint32_t Remainder_from_block = Sending_Size%Buffer_Size;
-
-if(Sending_in_Block != 0)
-{
-	for(uint32_t j = 0; j < (Sending_in_Block); j++)
-		{
-		HAL_SPI_Transmit(HSPI_INSTANCE, (unsigned char *)burst_buffer, Buffer_Size, 10);	
-		}
-}
-
-//REMAINDER!
-HAL_SPI_Transmit(HSPI_INSTANCE, (unsigned char *)burst_buffer, Remainder_from_block, 10);	
-	
-HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_SET);
+	//REMAINDER!
+	HAL_SPI_Transmit(HSPI_INSTANCE, (unsigned char *)burst_buffer, Remainder_from_block, 10);	
+		
+	HAL_GPIO_WritePin(LCD_CS_PORT, LCD_CS_PIN, GPIO_PIN_SET);
 }
 
 void LCD_Fill_Screen(uint16_t Color)
 {
-LCD_Set_Address(0,0,LCD_WIDTH,LCD_HEIGHT);	
-LCD_Draw_Color_Burst(Color, LCD_WIDTH*LCD_HEIGHT);	
+	LCD_Set_Address(0,0,LCD_WIDTH,LCD_HEIGHT);	
+	LCD_Draw_Color_Burst(Color, LCD_WIDTH*LCD_HEIGHT);	
 }
 
